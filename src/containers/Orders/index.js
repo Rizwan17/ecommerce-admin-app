ea574da4a54dc5b65acccde0906a49fa179f8e74
Rizwan Khan <rizwankhan.august16@gmail.com>
2020-12-09 10:13:46 -0800

    prettier

--- src/containers/Orders/index.js
+++ src/containers/Orders/index.js
@@ -96,28 +96,6 @@ const Orders = (props) => {
                   </div>
                 </div>
               ))}
-
-              {/* <div className="orderStatus">
-                <div className="point"></div>
-                <div className="orderInfo">
-                  <div className="status">Packed</div>
-                  <div className="date">Fri, 2020</div>
-                </div>
-              </div>
-              <div className="orderStatus">
-                <div className="point"></div>
-                <div className="orderInfo">
-                  <div className="status">Shipped</div>
-                  <div className="date">Fri, 2020</div>
-                </div>
-              </div>
-              <div className="orderStatus">
-                <div className="point"></div>
-                <div className="orderInfo">
-                  <div className="status">Delivered</div>
-                  <div className="date">Fri, 2020</div>
-                </div>
-              </div> */}
             </div>
 
             {/* select input to apply order action */}
